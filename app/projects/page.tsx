f659e87a472fdbf8f7adfcc39e126e35ce5ec7d9
Josh Pigford <josh@joshpigford.com>
2024-01-24 14:19:32 -0600

    Projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,12 +9,35 @@ export const metadata: Metadata = {
 export default async function Page() {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore);
-  const { data: projects } = await supabase.from("projects").select();
+  const { data: projects } = await supabase.from("projects").select().order('year', { ascending: true });
 
   return (
-    <div className="prose dark:prose-invert font-serif">
-      <h1 className="font-serif text-4xl">Projects</h1>
-      <p className="text-lg">Slowly backfilling content from the previous site. And by &quot;slowly&quot; I mean &quot;in a few years&quot; or &quot;never&quot;. 😬</p>
+    <div className="prose dark:prose-invert font-serif max-w-full w-full">
+      <div>
+        <h1 className="font-serif text-4xl">Projects</h1>
+        <p className="text-lg">I&apos;ve worked on (and continue to work on) an absurd number of things. Current tally? {projects?.length} projects.</p>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Started</th>
+            <th>Description</th>
+            <th>What Happened</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects?.map((project) => (
+            <tr key={project.id} className="">
+              <td className="px-2 py-3">{project.name}</td>
+              <td>{project.year}</td>
+              <td>{project.description}</td>
+              <td>{project.outcome}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
