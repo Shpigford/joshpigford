af6d2601b66b16bb48503a8c4324d41514b26276
Josh Pigford <josh@joshpigford.com>
2024-01-24 11:19:59 -0600

    Initi

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="prose dark:prose-invert font-serif">
+      <h1 className="font-serif text-4xl">Projects</h1>
+      <p className="text-lg">Slowly backfilling content from the previous site. And by &quot;slowly&quot; I mean &quot;in a few years&quot; or &quot;never&quot;. 😬</p>
+    </div>
+  )
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
@@ -8,13 +9,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        serif: ["Gambarino-Regular", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        white: "#eee",
+        black: "#111",
+        gold: {
+          "50": "#ffffe7",
+          "100": "#ffffc1",
+          "200": "#fffb86",
+          "300": "#fff041",
+          "400": "#ffe00d",
+          "500": "#ffd100",
+          "600": "#d19900",
+          "700": "#a66d02",
+          "800": "#89550a",
+          "900": "#74450f",
+          "950": "#442404",
+        },
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
