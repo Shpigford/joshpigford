c4efd30bbeab1770ef3e04e488177c21dd586247
Josh Pigford <josh@joshpigford.com>
2024-01-24 14:03:08 -0600

    Initial supabase foundation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,16 @@
 import { Metadata } from 'next'
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
 
 export const metadata: Metadata = {
   title: 'Projects',
 }
 
-export default function Page() {
+export default async function Page() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore);
+  const { data: projects } = await supabase.from("projects").select();
+
   return (
     <div className="prose dark:prose-invert font-serif">
       <h1 className="font-serif text-4xl">Projects</h1>
