65bbe825397ec8fcb060195296e73ea3a02dccf9
Josh Pigford <josh@joshpigford.com>
2024-01-27 13:48:21 -0600

    mission control bits

--- app/(reader)/page.tsx
+++ app/(reader)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="prose dark:prose-invert font-serif">
-      <p className="text-3xl">Dabbler. Currently building <a href="https://maybe.co" className="thick-link">Maybe</a>, an OS for your personal finances.</p>
+      <p className="text-3xl">Dabbler. Currently building <a href="https://maybe.co" className="thick-link">Maybe</a>, the OS for your personal finances.</p>
       <p className="text-2xl">You can find me on <a href="https://twitter.com/Shpigford" className="thick-link">Twitter</a>. I really spend far too much time there.</p>
     </div>
   );
--- app/mission-control/layout.tsx
+++ app/mission-control/layout.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import "@/app/globals.css";
 import { RandomSymbol } from "@/app/lib/RandomSymbol";
 import { Analytics } from '@vercel/analytics/react';
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: {
@@ -11,11 +13,15 @@ export const metadata: Metadata = {
   }
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const user = await supabase.auth.getUser();
+
   return (
     <html lang="en" className="dark">
       <body className="dark:bg-black dark:text-white p-8 md:py-20">
@@ -28,6 +34,18 @@ export default function RootLayout({
             <header className="prose dark:prose-invert font-serif mb-10">
               <h1 className="text-6xl font-light tracking-tight mb-0">Josh Pigford</h1>
               <h2 className="text-3xl font-light tracking-tight my-3">Mission Control</h2>
+
+              {user && user.data && user.data.user !== null && (
+                <p className="text-xl">
+                  <Link
+                    href="/articles"
+                    className="thick-link"
+                  >Articles</Link> / <Link
+                    href="/projects"
+                    className="thick-link"
+                  >Projects</Link>
+                </p>
+              )}
             </header>
             <div>{children}</div>
           </div>
