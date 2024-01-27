3e1caaf5fdf1b4577003b94be18c5127f1ecf659
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2024-01-27 03:28:04 +0000

    Working login/logout

--- app/mission-control/page.tsx
+++ app/mission-control/page.tsx
@@ -1,21 +1,12 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+import AuthButton from "./AuthButton";
+import { cookies } from "next/headers";
 
-import { createClient } from '@/utils/supabase/server'
-
-export default async function MissionControlPage() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-
-  const { data, error } = await supabase.auth.getUser()
-  if (error || !data?.user) {
-    redirect('/mission-control/login')
-  }
+export default async function Index() {
+  const cookieStore = cookies();
 
   return (
     <div>
-      <p>Hello {data.user.email}</p>
+      <AuthButton />
     </div>
-  )
+  );
 }
-
--- middleware.ts
+++ middleware.ts
@@ -1,62 +1,25 @@
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
+import { createClient } from "./utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
+  try {
+    const { supabase, response } = createClient(request);
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-        },
+    // Refresh session if expired - required for Server Components
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+    await supabase.auth.getSession();
+
+    return response;
+  } catch (e) {
+    // If you are here, a Supabase client could not be created!
+    // This is likely because you have not set up environment variables.
+    // Check out http://localhost:3000 for Next Steps.
+    return NextResponse.next({
+      request: {
+        headers: request.headers,
       },
-    }
-  );
-
-  await supabase.auth.getUser();
-
-  return response;
+    });
+  }
 }
 
 export const config = {
